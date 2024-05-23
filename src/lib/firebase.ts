fb5d9ba6abbff231459735e8ecc24abc45d83f80
shiki-01 <0110shiki@gmail.com>
2024-05-23 22:16:04 +0900

    🐛 fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { config } from "dotenv";
+import { getFirestore, type Firestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-config();
-
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: process.env.APP_FIREBASE_API_KEY,
-    authDomain: process.env.APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.APP_FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db: Firestore = getFirestore(app);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,72 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import {
+		addDoc,
+		collection,
+		onSnapshot,
+		query,
+		QuerySnapshot,
+		serverTimestamp,
+		FieldValue,
+		orderBy,
+		deleteDoc,
+		doc
+	} from 'firebase/firestore';
+	import { db } from '$lib/firebase';
+
+	type Item = {
+		id?: string;
+		name: string;
+		timestamp: FieldValue;
+	};
+
+	let itemName: string = '';
+	let wishList: Item[] = [];
+
+	function addItem() {
+		if (itemName == '') return;
+		const item: Item = {
+			name: itemName,
+			timestamp: serverTimestamp()
+		};
+		addDoc(collection(db, 'wishlist'), item);
+		itemName = '';
+	}
+
+	function delItem(item: Item) {
+		if (!item.id) return;
+		deleteDoc(doc(db, 'wishlist', item.id));
+	}
+
+	onSnapshot(
+		query(collection(db, 'wishlist'), orderBy('timestamp', 'desc')),
+		(snapshot: QuerySnapshot): any => {
+			wishList = snapshot.docs.map((doc) => {
+				const data = doc.data();
+				const item: Item = {
+					id: doc.id,
+					name: data.name,
+					timestamp: data.timestamp
+				};
+				return item;
+			});
+		}
+	);
+</script>
+
+<section>
+	<div>
+		<h1>✅ Wish List</h1>
+		<div>
+			<input type="text" bind:value={itemName} />
+			<button on:click={addItem}>Add Item</button>
+		</div>
+		<ul>
+			{#each wishList as item}
+				<li>
+					<p><span>✔</span><span>{item.name}</span></p>
+					<button on:click={() => delItem(item)}>🗑️</button>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</section>
