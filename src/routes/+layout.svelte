6005fa41be1c13c57258fb51fb1405893f3be5f8
shiki <140712093+shiki-01@users.noreply.github.com>
2024-09-13 01:06:16 +0000

    save

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.css';
+	import '@fontsource/zen-kurenaido';
 	import { Navbar, NavBrand, NavUl, NavHamburger } from 'flowbite-svelte';
 	import { Footer, FooterBrand, FooterCopyright, FooterLinkGroup } from 'flowbite-svelte';
 	import { scrollTo, scrollTop } from 'svelte-scrolling';
@@ -62,7 +63,10 @@
 	</Footer>
 </div>
 
-<style>
+<style lang="postcss">
+	:global(body) {
+		font-family: 'Zen Kurenaido', sans-serif;
+	}
     .main {
         background-image: linear-gradient(0deg, rgb(20, 73, 117), rgb(115, 255, 255))
     }
