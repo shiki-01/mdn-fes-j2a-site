50cb9cee75faa29102f036a7526d1625924921d4
shiki <kishintorii@gmail.com>
2024-09-18 07:42:28 +0900

    deploy

--- src/routes/addranking/+page.svelte
+++ src/routes/addranking/+page.svelte
@@ -8,28 +8,18 @@
 	let result: string | null = null;
 	let key = import.meta.env.VITE_ENCRYPT_KEY
 
-	const a = 'U2FsdGVkX1+8XuZ7jvckFPAxJqko1Okfwi41/a2uARc='
-	let num = '200'
-	num = AES.encrypt(num,key).toString()
-	let nummm = ''
-
 	onMount(() => {
 		co = $page.url.searchParams.get('co') || '';
+		co = co.replace(/ /g, '+');
 
-		const bytes = AES.decrypt(co, key)
-		result = bytes.toString(Utf8)
-		const bytes2 = AES.decrypt(a, key)
-		nummm = bytes2.toString(Utf8)
-
-		if (false) {
+		if (co !== '') {
 			try {
-				const bytes = AES.decrypt(num, key);
+				const bytes = AES.decrypt(co, key);
 				result = bytes.toString(Utf8);
-				console.log('Decrypted:', result);
-				//if (!/^(0|[1-9]00)$/.test(result)) console.log((''));
+				if (!/^(0|[1-9]00)$/.test(result)) result = null;
 			} catch (error) {
 				console.error('Decryption failed:', error);
-				result = 'e';
+				result = null;
 			}
 		}
 	});
@@ -41,6 +31,8 @@
 		<p>もしすでにクラスを離れている場合は、クラス後方の出口の人に点数を教えて、再度QRコードを生成してもらってください。</p>
 	</div>
 {:else}
-	<h1>追加しました</h1>
-	<p>クラス名: {num},,,,,,{co},,,{result},,,,,{nummm}</p>
+	<div class="w-full h-screen flex flex-col justify-center text-center text-lg">
+		<h1>追加しました</h1>
+		<p>あなたの得点は…{result}</p>
+	</div>
 {/if}
